d4ec459c87cfaa8553cc723e1f3db19f3bb1aee5
Tony Brix <tony@brix.ninja>
2020-11-27 17:04:37 -0600

    chore(ci): use ci spec helper

--- spec/runner.js
+++ spec/runner.js
@@ -3,6 +3,7 @@ const { createRunner } = require('atom-jasmine3-test-runner')
 module.exports = createRunner({
 	specHelper: {
 		attachToDom: true,
+		ci: true,
 	},
 	testPackages: ['minimap'],
 })
